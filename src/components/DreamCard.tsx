e575b3486086ca16c7c9a7eb6ae7e296b5a9b95d
caleb swenson <calebswenson@calebs-MacBook-Pro.local>
2024-04-26 12:03:35 -0700

    fixed card

--- src/components/DreamCard.tsx
+++ src/components/DreamCard.tsx
@@ -3,6 +3,9 @@ import { DreamType, UserType } from '../types';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 type DreamCardProps = {
     dream: DreamType
     currentUser: UserType|null
@@ -19,17 +22,22 @@ export default function DreamCard({ dream, currentUser }: DreamCardProps) {
     }, [dream, currentUser])
     return (
         <Card className='my-3 bg-custom' text='black'>
-            <Card.Header>{ dream.dream_date }</Card.Header>
+            <Card.Header className="text-center">Posted on:{ dream.dream_date }</Card.Header>
             <Card.Body>
-                <Card.Title>{ dream.sleepStart }</Card.Title>
-                <Card.Text>{ dream.dream }</Card.Text>
-                <Card.Text>{ dream.sleepEnd }</Card.Text>
-                <Card.Text>{ dream.isPublic }</Card.Text>
-                <Card.Text>{ dream.keywords }</Card.Text>
-                <Card.Text>{dream.sleepStart}</Card.Text>
+                <Card.Title className="text-center">slept from:{ dream.sleepStart } - {dream.sleepEnd}</Card.Title>
+                <label htmlFor="dream" className="text-center">The Dream:</label>
+                <Card.Text name="dream"className="border-bottom p-2">{ dream.dream }</Card.Text>
+                <Card.Text className="text-center">This dream is { dream.isPublic }</Card.Text>
+                <Card.Text className="text-center">Keywords: { dream.keywords }</Card.Text>
+               <Row className="mx-5 justify-content-center align-items-center align-self-center">
+                <Col className="justify-content-center align-items-center align-self-center" >
                 {dream.author.id === currentUser?.id && <Link to={`/dreams/${dream.id}`}><Button variant='primary'>Edit Dream</Button></Link>}
+                </Col>
+                <Col className="justify-content-center align-items-center align-self-center">
                 {interpretable && <Link to={`/interpret/${dream.id}`}><Button variant='success'>Interpret Dream</Button></Link>}
-                <Card.Subtitle>Created By: { dream.author.username }</Card.Subtitle>
+                </Col>
+                </Row>
+                <Card.Subtitle className="text-center mt-2">Created By: { dream.author.username }</Card.Subtitle>
             </Card.Body>
         </Card>
     )
