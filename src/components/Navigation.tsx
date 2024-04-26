bdd66191bd982a43882808f4dce72ea5b9f30785
caleb swenson <calebswenson@calebs-MacBook-Pro.local>
2024-04-26 09:52:36 -0700

    remove theme button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+
 
 type NavigationProps = {
     isLoggedIn: boolean
@@ -12,7 +12,7 @@ type NavigationProps = {
 
 export default function Navigation({ isLoggedIn, logUserOut }: NavigationProps){
 
-    const [backgroundTheme, setBackgroundTheme] = useState('dark');
+    const [backgroundTheme] = useState('light');
     
     return (
         <Navbar expand='lg' data-bs-theme={backgroundTheme} bg={backgroundTheme}>
@@ -35,7 +35,6 @@ export default function Navigation({ isLoggedIn, logUserOut }: NavigationProps){
                         )}
                     </Nav>
                     <Nav>
-                        <Button onClick={() => setBackgroundTheme(backgroundTheme === 'dark' ? 'light' : 'dark')}>Change Background</Button>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
